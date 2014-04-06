7a1cd7c5d20b38a8c178fb6c0319c4127082f807
Jameson Trinker <jameson.trinker@gmail.com>
2014-04-06 16:58:10 -0500

    implement sign up and update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :get_user, only: [:show, :edit, :update, :destroy]
+	
 	def new
 		@user = User.new
 	end
@@ -17,7 +19,24 @@ class UsersController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Profile has been updated."
+  			redirect_to "/"
+  		else
+  			flash[:error] = "Profile failed to update."
+  			render "edit"
+  		end
+	end
+
 	private
+
+	def get_user
+		@user = User.find(params[:id])
+	end
 
 	def user_params
 		params.require(:user).permit(:name, :password, :password_confirmation)
